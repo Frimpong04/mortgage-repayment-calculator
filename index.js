b0831865263ff0c82c0728be90c73e281b81113b
Kwame Frimpong-Boateng <frimpong@Kwames-MacBook-Air.local>
2024-09-05 22:00:34 +0000

    fix mortgage calculation errors

--- index.js
+++ index.js
@@ -30,9 +30,22 @@ const termError = document.querySelector(".term-input-error");
 const rateError = document.querySelector(".rate-input-error");
 const radioInputError = document.querySelector(".radio-input-error");
 
+// results element
+const repaymentAmount = document.querySelector(".monthly-repayment-amount");
+const grandTotal = document.querySelector(".grand-total");
+const grandTotalAmount = document.querySelector(".grand-total-amount");
+const resultsContainer = document.querySelector(".results-container");
+const emptyContainer = document.querySelector(".empty-results-container");
+
+const paymentTitle = document.querySelector(".payment-title");
+const totalPayment = document.querySelector(".total-title");
+
+let interesetOption ;
+let repaymentOption ;
 
 // clearAll to reset the form
 clearAllEl.addEventListener("click", () => {
+
     console.log("reset clicked");
     amountInputEl.value = null;
     termInputEl.value = null;
@@ -44,6 +57,9 @@ clearAllEl.addEventListener("click", () => {
     interesetRadioEl.checked = false;
     repaymentRadioContainerEl.classList.remove("selected");
     interestContainerEl.classList.remove("selected");
+
+    resultsContainer.style.display = "none";
+    emptyContainer.style.display = "block";
 
 });
 
@@ -70,6 +86,7 @@ rateInputEl.addEventListener("focus", () => {
 repaymentRadioContainerEl.addEventListener("click", () => {
     console.log("repayment");
     repaymentRadioEL.checked = true;
+    repaymentOption = repaymentRadioEL.value;
     interesetRadioEl.checked = false;
     repaymentRadioContainerEl.classList.add("selected");
     interestContainerEl.classList.remove("selected");
@@ -81,6 +98,7 @@ interestContainerEl.addEventListener("click", ()=> {
     repaymentRadioContainerEl.classList.remove("selected");
     interestContainerEl.classList.add("selected");
     interesetRadioEl.checked = true;
+    interesetOption = interesetRadioEl.value;
     repaymentRadioEL.checked = false
     radioInputError.style.display = "none";
 });
@@ -92,13 +110,12 @@ formEl.addEventListener("submit", (e) => {
     const amountInputVlaue = amountInputEl.value;
     const termInputValue = termInputEl.value;
     const rateInputElValue = rateInputEl.value;
-
+   
 
 
     if(!amountInputVlaue) {
         amountContainerEl.classList.add("error-state");
         amountError.style.display = "block";
-        
     }
 
     if(!termInputValue) {
@@ -113,13 +130,56 @@ formEl.addEventListener("submit", (e) => {
         
     }
 
-    if(!interesetRadioEl.value || !repaymentRadioEL.value) {
+    if(!interesetOption && !repaymentOption) {
         radioInputError.style.display = "block";
-    
     }
 
     console.log(amountInputVlaue,termInputValue,rateInputElValue);
+    // R = P * r / (1 - (1 / (1 + r)n))
+
+    let monthlyinterest = rateInputElValue / 12;
+    let num = 1 + monthlyinterest;
+    let monthlyRepayment = ((amountInputVlaue * monthlyinterest) / (1 - (1 / (num * termInputValue * 12)))).toFixed(2);
+    let overallAmount = (monthlyRepayment * termInputValue ).toFixed(2);
+   
+
+    if(repaymentRadioEL.checked && amountInputVlaue && termInputValue && rateInputElValue) {
+        console.log(repaymentRadioEL.value);
+        paymentTitle.textContent = "Monthly repayment";
+        totalPayment.textContent = "Total you'll repay over the term"
+        repaymentAmount.textContent = monthlyRepayment;
+        grandTotalAmount.textContent = overallAmount;
+        emptyContainer.style.display = "none";
+        resultsContainer.style.display = "block";
+    }
+
+    if(interesetRadioEl.checked && amountInputVlaue && termInputValue && rateInputElValue) {
+        console.log(interesetRadioEl.value)
+        paymentTitle.textContent = "Monthly Interest payment";
+        totalPayment.textContent = "Total interest you'll repay over the term"
+        // total intereset = (yearly interest /100) * total amount * term
+        // monthly interest = total interset / term * 12 
+        const interestPayment =  ((rateInputElValue /100) * amountInputVlaue * termInputValue).toFixed(2); 
+        const monthlyInterestpayment = (interestPayment / (termInputValue * 12)).toFixed(2);
+
+        grandTotalAmount.textContent = interestPayment;
+        repaymentAmount.textContent = monthlyInterestpayment;
+        emptyContainer.style.display = "none";
+        resultsContainer.style.display = "block";
+    }
+    
+
+});
 
 
 
-})
+// From the principal amount, the interest rate, and the loan period you will calculate the monthly payment, the sum of all payments and the total amount paid in interest. The formula is given below:
+
+//    R = P * r / (1 - (1 / (1 + r)n))
+
+// where
+
+// R = monthly payment
+// P = principal loan amount
+// r = monthly interest rate (yearly rate divided by 12)
+// n = number of months
